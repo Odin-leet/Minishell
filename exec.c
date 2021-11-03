77033ff41e4a6a908f8cba03d33a4ea3a2366eb3
heskanor <shiteahmed@gmail.com>
2021-11-03 17:05:22 +0100

    pipes

--- exec.c
+++ exec.c
@@ -1,17 +1,19 @@
 #include "minishell.h"
 
-void whoexec()
+void whoexec(t_linked_list cmd)
 {
-
+    
 }
 
 int exec(t_linked_list *head)
 {
 	int i;
+	int fd[2];
 
 	i = 0;
 	t_linked_list *lcmd;
 	t_linked_list *lfile;
+	pipe(fd);
 	
 	while (head != NULL)
 	{
@@ -22,18 +24,64 @@ int exec(t_linked_list *head)
 			if (lcmd != NULL)
 			{
 				printf("-%d\ncmd : %s\n",i,(char *)lcmd->data);
-				whoexec();
+				
 				lcmd = lcmd->next;
 			}	
 			if (lfile != NULL)
 			{
 				printf("- %d \nfile : %s\ntype : %d\n",i,((t_file*)lfile->data)->file,((t_file*)lfile->data)->type);
+				whoexec(head);
 				lfile = lfile->next;
 			}
 			i++;
 		}
-		printf("there is pipe here  \n");
 		head = head->next;
+		printf("there is pipe here  \n");
+
 	}
     return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	char *args[3] = {"/bin/ls", "/usr/bin/wc"};
+	pid_t pid;
+	int fd[2];
+	int in = 0;
+	int pin = 0;
+	int out = 1;
+
+	pipe(fd);
+	pin = fd[0];
+	out = fd[1];
+	for(int i = 0; i < 2; i++)
+	{
+		if (i == 1)
+			out = 1;
+		pid = fork();
+		if (pid == 0)
+		{
+			dup2(in, 0);
+			dup2(out, 1);
+			if (pin != 0)
+				close(pin);
+			if (in != 0)
+				close(in);
+			if (out != 1)
+				close(out);
+			execve(args[i], &argv[i], 0);
+		}
+		else
+		{
+			if (out != 1)
+				close(out);
+			if (in != 0)
+				close(in);
+			in = pin;
+		}
+	}
+	return (0);
 }
