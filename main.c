a6c3cbe9b9db14e3e032892191e4f155e4d3bd90
heskanor <shiteahmed@gmail.com>
2021-11-13 21:53:25 +0100

    D

--- main.c
+++ main.c
@@ -7,33 +7,24 @@
 
 int main(int argc, char **argv,char **env)
 {
-	char *args[3] = {"/bin/ls", "-la", 0};
+	char *args[3] = {"/bin/ls", "/usr/bin/wc", NULL};
 	pid_t pid;
 	int fd[2];
-	int in = 0;
-	int pin = 0;
-	int out = 1;
-
+	int stdin = 0;  //input is read from somewhere
+	int stdout = 1; //output is written somewhere
+	int stderr = 2;
+	int i = 0;
+// don't turn left :)
 	pipe(fd);
-	pin = fd[0];
-	out = fd[1];
-	for(int i = 0; i < 2; i++)
+	while (i < 1)
 	{
-		/*last cmd out should be [1]
-		*/
-		if (i == 1)
-			out = 1;
 		pid = fork();
 		if (pid == 0)
 		{
-			dup2(in, 0);
-			dup2(out, 1);
-			if (pin != 0)
-				close(pin);
-			if (in != 0)
-				close(in);
-			if (out != 1)
-				close(out);
+			dup2(fd[0], stdin);
+			dup2(fd[1], stdout);
+			if (fd[1] != 1)
+				close(fd[1]);
 			printf("%s nothing here\n", argv[i]);
 			execve(args[0], args, env);
 			// if (execve(args[i], &argv[i], 0) != 1)
